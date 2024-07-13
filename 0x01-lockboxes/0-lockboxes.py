eb0ce59db2f8a9e03a9e8ad037b3829ce4d29443
gregorymark254 <gregorymark254@gmail.com>
2024-07-13 15:19:34 +0300

    lock

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+"""determine if all the boxes can be opened"""
 
 
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    """
+    Return: True or False
+    """
+    a = len(boxes)
+    stack = [0]
+    unlocked_box = [1] + [0] * (a - 1)
+    i = 0
+
+    if a == 0:
+        return True
+    while stack:
+        j = stack.pop()
+        for index in boxes[j]:
+            if index > 0 and index < a and unlocked_box[index] == 0:
+                unlocked_box[index] = 1
+                stack.append(index)
+        i = i + 1
+    if 0 in unlocked_box:
+        return False
+    return True
